ff1102012c7d79c9ef9c6d4e724b38a8778d9469
mlvrkhn <martin.guide@gmail.com>
2021-02-28 15:16:52 +0100

    Move useEffect to SearchPanel

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,10 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
 import GlobalStyle from '../styled/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import theme from '../styled/theme';
 import SearchPanel from './SearchPanel';
-import { getDataFromRegister } from '../modules/brreg/actions';
 
 const App = () => {
-    const dispatch = useDispatch();
-    const query = useSelector((state) => state.query);
-
-    useEffect(() => {
-        if (query.length > 2) {
-            dispatch(getDataFromRegister());
-        }
-    }, [query]);
-
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import StyledSearchPanel from './SearchPanel.styled';
+import { getDataFromRegister } from '../../modules/brreg/actions';
 import Header from '../Header';
 import Results from '../Results';
 
 const SearchPanel = () => {
+    const dispatch = useDispatch();
+    const query = useSelector((state) => state.query);
+
+    useEffect(() => {
+        if (query.length > 2) {
+            dispatch(getDataFromRegister());
+        }
+    }, [query]);
+
     return (
         <StyledSearchPanel>
             <Header />
